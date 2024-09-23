379529bc51cdc77b93708160faa6bb0527d5ef18
sschalkowski <silvan.schalkowski@doubleslash.de>
2024-09-23 10:08:01 +0200

    Changed: - Merged both ts configs into one
    Added:
    - ts type for env variable

--- src/glb-services/api.ts
+++ src/glb-services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `http://${process.env.BACKEND_HOST}:9090`,
+
+  baseURL: `http://${import.meta.env.VITE_BACKEND_HOST}:9090`,
   headers: {
     'Content-Type': 'application/json',
   },
