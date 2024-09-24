df1740c41e5a1da2a50fcdf09913a371edc530a2
sschalkowski <silvan.schalkowski@doubleslash.de>
2024-09-24 08:58:35 +0200

    Changed: - Port to 30001 for service communication in k8s

--- src/glb-services/api.ts
+++ src/glb-services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
 
-  baseURL: `${window.runtimeConfig.BACKEND_HOST}:9090`,
+  baseURL: `${window.runtimeConfig.BACKEND_HOST}:30001`,
   headers: {
     'Content-Type': 'application/json',
   },
